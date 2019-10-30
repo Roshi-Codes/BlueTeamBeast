1966d744a12c9a04d44859848f1a0cb9e4cdb8c1
Callum <49819785+Roshi-Codes@users.noreply.github.com>
2019-10-30 11:25:48 +0000

    Add files via upload

---
+++ PowerShell/Deploy Agents/Splunk/deploy_splunk_forwarder.ps1
@@ -0,0 +1,43 @@
+﻿# This is designed to be executed in conjunction with install_Splunk.ps1, it will not work without this and a list of target machines.
+# Either IPs or hostnames IF the machine you are running it from uses the domain dns! If not you must use IP addresses!
+
+# This gets the current working directory 
+$loc = Get-Location
+
+# The file containing target IPs or Target hostnames is appended to the path.
+# For this to work the targets file must be in the same directory as this script.
+$TargetLoc = $loc.Path + "\targets.txt"
+
+# If you are using a different directory for your file containing targets comment out the line assigning $TargetLoc above.
+# Then replace the below $TargetLoc with the location of the list of IPs/host names to invoke the command on.
+$computers = (get-content -path $TargetLoc)
+
+# The install_Splunk script should be in the same folder as this script.
+$SplunkLoc = $loc.Path + "\install_Splunk.ps1"
+
+# You will be prompted for your username and password when you run the script. !!!NEVER STORE YOUR PASSWORD IN PLAINTEXT!!!
+# If you do not want to type in your usename each time you run the script, uncomment and replace the credentials below with the domian name and user name
+$creds = Get-Credential # -Credential domain\user 
+
+$status = @()
+
+# This block will loop through the computers in targets.txt, first installing the forwarder, then attempting to get the service status
+# The $comp and service status will be added to the $status array. The session and $return variable are then cleaned up 
+foreach ($comp in $computers)
+{
+echo "Connecting to $comp"
+$s = New-PSSession -ComputerName $comp -Credential $creds
+Invoke-Command -Session $s -FilePath $SplunkLoc
+$return = $(Invoke-Command -Session $s -ScriptBlock {(Get-Service SplunkForwarder).Status} 2> $null)
+if ($return.length -eq 0) {$return = "Service not found!"}
+$status.Add("$($comp) - $($return)")
+echo "Closing session"
+Remove-PSSession -Session $s
+Remove-Variable $return
+}
+
+# $status is output to both a log file and the terminal to end the script
+$statusPath = $loc.path + "\Forwarders_Status_Log.txt"
+$status | out-file $statusPath
+echo $status
+echo "End of script"
---
+++ PowerShell/Deploy Agents/Splunk/install_Splunk.ps1
@@ -0,0 +1,78 @@
+﻿# Read the comments - You must make changes for this to work in your environment.
+
+function filedownload {
+echo "Downloading Splunk Forwarder..."
+
+# You must replace the below url with the url and name of your hosted splunkforwarder - simple python http server will work.
+$url = "http://<URL>/splunkforwarder.msi" 
+
+# Path that the msi is saved to on the end host - If you change this you must change the location to match thoughout the rest of the script.
+$path = "C:\splunkforwarder.msi" 
+
+# Downloads the file from the url and saves to the path - This version is PowerShell V2 compatible
+(New-Object Net.WebClient).DownloadFile($url, $path) 
+
+# If you are hosting the splunk msi on a network share, comment out the New-Object command above with a hashtag
+# Then uncomment and edit the below line with the network share source.
+##Copy-Item "\\change\me\splunkforwarder.msi" -Destination $path
+
+# Testing file was created
+filetest
+if ($test = "False"){Write-Warning "Download unsucessful, Check connectivity and file availibilty, then try installing on $env:COMPUTERNAME again. Leaving session with $env:COMPUTERNAME.."; Exit}
+}
+
+
+function filetest {
+Global $test = Test-Path C:\splunkforwarder.msi
+if ($test = "True"){Continue}
+elif ($test = "False"){Write-Warning "C:\splunkforwarder.msi NOT Found"}
+}
+
+
+function fileinstall {
+echo "Installing on $env:COMPUTERNAME, this can take a long time in virtual environments with low resources! Please wait..."
+
+# You must replace the arguements with the areguements you want to run on your own splunk forwarder, leave the '/quiet' at the end
+# MAKE SURE YOU CHANGE YOUR RECEIVING_INDEXER & DEPLOYMENT_SERVER IPs!!! 
+Start-Process msiexec.exe -wait '/I C:\splunkforwarder.msi AGREETOLICENSE=yes RECEIVING_INDEXER="<IP>:<PORT>" DEPLOYMENT_SERVER="<IP>:<PORT>" WINEVENTLOG_APP_ENABLE=1 WINEVENTLOG_SEC_ENABLE=1 WINEVENTLOG_SYS_ENABLE=1 WINEVENTLOG_FWD_ENABLE=1 WINEVENTLOG_SET_ENABLE=1 SPLUNKUSER=admin GENRANDOMPASSWORD=1 ENABLEADMON=1 /quiet'
+}
+
+
+function servicecheck {
+echo "Checking service was created.."
+if ($ser = Get-Service SplunkForwarder) 
+{ 
+    Echo $ser.Status 
+    if ( $ser.Status -ne "Running")
+    { Start-Service splunkForwarder }
+}
+else { 
+servicenotfound
+}
+}
+
+
+function servicenotfound {
+Write-Warning "Service not found, re-trying install.."
+fileinstall
+if ($ser = Get-Service SplunkForwarder) 
+{ 
+    Echo $ser.Status 
+    if ( $ser.Status -ne "Running")
+    { Start-Service splunkForwarder }
+} 
+else {Write-Warning "Service still not found, ensure you have the correct permissions to install a service and that all the commandline arguements in the script are correct! Leaving session with $env:COMPUTERNAME.."; Exit}
+}
+
+
+function filedelete {
+# The install file is deleted after install
+echo "Cleaning up.."
+del C:\splunkforwarder.msi
+echo "$env:COMPUTERNAME clean."
+}
+
+filedownload
+fileinstall
+servicecheck
+filedelete
