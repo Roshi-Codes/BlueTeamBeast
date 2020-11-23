159491060f6a02fbecf17c654c20f40d04292787
Roshi <49819785+Roshi-Codes@users.noreply.github.com>
2020-11-23 12:38:06 +0000

    Added PresentationFramework

--- Powershell/1. Preparation/Get-PSVersion.ps1
+++ Powershell/1. Preparation/Get-PSVersion.ps1
@@ -48,6 +48,7 @@
 
 #----------------------------------------------------[Imports]----------------------------------------------------
 
+Add-Type -AssemblyName PresentationFramework
 Add-Type -AssemblyName System.Windows.Forms
 [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 
